ba0ba6791d12059391078f71dfbba32e688876a6
Herson Castillo <hersoncastillo12@hotmail.com>
2019-08-08 20:57:38 -0600

    vueify

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="home">
+        <p>Hola Mundo</p>
+    </div>
+</template>
+
+<script lang="ts" src="./Home.ts"></script>
+<style lang="scss" scoped>
+    @import url('./Home.scss');
+</style>
