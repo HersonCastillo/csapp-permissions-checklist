f97a1e98f63cacee875222f9bb6628ce3c561bf1
Herson Castillo <hcastillo@red4g.net>
2019-08-14 08:07:54 -0600

    home download apk

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,11 +1,60 @@
 <template>
     <div>
-        <p>Hola Mundo!</p>
+        <v-btn 
+            dark
+            class="center"
+            @click="downloadFile"
+        >
+            You want it? take it
+        </v-btn>
     </div>
 </template>
 
 <!--Scripts-->
-<script lang="ts" src="./Home.ts"></script>
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class Home extends Vue {
+        public downloadurl = {
+            version: null,
+            status: null,
+            dateVersion: null
+        };
+        constructor(){
+            super();
+            setTimeout(() => {
+                const url: string = 'https://csapp.red5g.com/auth/app';
+                fetch(url, {
+                    method: 'get',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    }
+                }).then(r => r.json()).then(r => {
+                    if(r.app_name){
+                        this.downloadurl.version        = r['current_version'];
+                        this.downloadurl.status         = r['status'];
+                        this.downloadurl.dateVersion    = r['version_date'];
+                    }
+                });
+            }, 1);
+        }
+        downloadFile(): void {
+            const baseurl: string   = 'https://csapp.red5g.com/apk/download/';
+            const version           = this.downloadurl.version;
+            const dateVersion       = this.downloadurl.dateVersion;
+            const status            = this.downloadurl.status;
+            const url: string       = baseurl.concat(`${version}/${dateVersion}/${status}`);
+            window.open(url);
+        }
+    }
+</script>
 <style lang="scss" scoped>
-    @import url('./Home.scss');
+    .center{
+        position: absolute;
+        transform: translate(-50%, -50%);
+        left: 50%;
+        top: 50%;
+    }
 </style>
